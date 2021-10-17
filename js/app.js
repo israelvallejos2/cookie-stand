51c5817eb6c96ba7eccb7eab482efb59537844c9
Israel Vallejo <vallejo.israel1996@gmail.com>
2021-10-16 22:51:54 -0700

    first attemp

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 // object for my Seattle Store
 const seattleStore = document.getElementById('seattleList');
 let hours = ['6 a.m.', '7 a.m.', '8 a.m.', '9 a.m.', '10 a.m.', '11 a.m.', '12 p.m.', '1 p.m.', '2 p.m.', '3 p.m.', '4 p.m.', '5 p.m.', '6 p.m.', '7 p.m.',];
-
+let cookies = 'cookies';
 let seattle = {
     name: 'Seattle',
     min: 23,
